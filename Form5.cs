11fcc0b14fb0acc8bce39078068ef2b9645a9740
Siber0817 <adam.nezveda@seznam.cz>
2024-02-14 18:44:58 +0100

    Předělání motivace na braní z .txt

--- Form5.cs
+++ Form5.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MultiApka
 {
@@ -45,53 +46,73 @@ namespace MultiApka
 
         private void button4_Click(object sender, EventArgs e)
         {
-            Random r = new Random();
-            int rInt = r.Next(0, 10);
+            string fileloc = "./motivace.txt";
+            
 
-            if (rInt == 0)
+            using (StreamReader read = new StreamReader(fileloc))
             {
-                MessageBox.Show("Na cestě k úspěchu je mnoho překážek. Proto se snaž, aby jsi se nestal/a jednou z nich.");
+
+                string[] lines = File.ReadLines(fileloc).ToArray();
+                //string line;
+                int i = lines.Length;
+                //while ((line = read.ReadLine()) != null)
+                //{                    
+                  //string arr[i] = line;
+                  //i++;
+                  //  Console.WriteLine(line);
+                //}
+                Random r = new Random();
+                int rInt = r.Next(0, i);
+                MessageBox.Show(lines[rInt]);
             }
-            else if (rInt == 1)
-            {
-                MessageBox.Show("Dokud nezměníme stav své mysli, budeme znovu a znovu vytvářet v podstatě stejný svět, se stejnými démony a stejně šílený.");
-            }
-            else if (rInt == 2)
-            {
-                MessageBox.Show("Pokud si nebuduješ svůj vlastní sen, buduješ sen někoho jiného.");
-            }
-            else if (rInt == 3)
-            {
-                MessageBox.Show("Zázraky se stávají lidem, kteří na ně věří. Nikdy na ně nepřestaň věřit");
-            }
-            else if (rInt == 4)
-            {
-                MessageBox.Show("Ten, kdo padne a znovu vstane, je mnohem silnější než ten, kdo nikdy nespadnul.");
-            }
-            else if (rInt == 5)
-            {
-                MessageBox.Show("To, že se usmívám neznamená, že je všechno pořád podle mých představ. Úsměv je symbolem mé síly a naděje.");
-            }
-            else if (rInt == 6)
-            {
-                MessageBox.Show("Nejdůležitější krok je ten první.");
-            }
-            else if (rInt == 7)
-            {
-                MessageBox.Show("Nemůžete změnit svůj život, dokud nezměníte něco, co děláte každý den. Úspěch je o malých krocích.");
-            }
-            else if (rInt == 8)
-            {
-                MessageBox.Show("Život není o tom čekat, až bouřka přejde, je dobré naučit se tančit v dešti.");
-            }
-            else if (rInt == 9)
-            {
-                MessageBox.Show("Nikdy nevzdávej své sny jen kvůli názoru jiných.");
-            }
-            else if (rInt == 10)
-            {
-                MessageBox.Show("Neúspěch nastane pouze ve chvíli, kdy se vzdáš. Vždy můžeš zkusit ještě něco udělat znovu jinak, objevit novou cestu.");
-            }
+
+            
+
+            //if (rInt == 0)
+            //{
+              //  MessageBox.Show("Na cestě k úspěchu je mnoho překážek. Proto se snaž, aby jsi se nestal/a jednou z nich.");
+            //}
+            //else if (rInt == 1)
+            //{
+                //MessageBox.Show("Dokud nezměníme stav své mysli, budeme znovu a znovu vytvářet v podstatě stejný svět, se stejnými démony a stejně šílený.");
+            //}
+            //else if (rInt == 2)
+            //{
+              //  MessageBox.Show("Pokud si nebuduješ svůj vlastní sen, buduješ sen někoho jiného.");
+            //}
+            //else if (rInt == 3)
+            //{
+              //  MessageBox.Show("Zázraky se stávají lidem, kteří na ně věří. Nikdy na ně nepřestaň věřit");
+            //}
+            //else if (rInt == 4)
+            //{
+              //  MessageBox.Show("Ten, kdo padne a znovu vstane, je mnohem silnější než ten, kdo nikdy nespadnul.");
+            //}
+            //else if (rInt == 5)
+            //{
+              //  MessageBox.Show("To, že se usmívám neznamená, že je všechno pořád podle mých představ. Úsměv je symbolem mé síly a naděje.");
+            //}
+            //else if (rInt == 6)
+            //{
+              //  MessageBox.Show("Nejdůležitější krok je ten první.");
+            //}
+            //else if (rInt == 7)
+            //{
+              //  MessageBox.Show("Nemůžete změnit svůj život, dokud nezměníte něco, co děláte každý den. Úspěch je o malých krocích.");
+            //}
+            //else if (rInt == 8)
+            //{
+              //  MessageBox.Show("Život není o tom čekat, až bouřka přejde, je dobré naučit se tančit v dešti.");
+            //}
+            //else if (rInt == 9)
+            //{
+              //  MessageBox.Show("Nikdy nevzdávej své sny jen kvůli názoru jiných.");
+            //}
+            //else if (rInt == 10)
+            //{
+              //  MessageBox.Show("Neúspěch nastane pouze ve chvíli, kdy se vzdáš. Vždy můžeš zkusit ještě něco udělat znovu jinak, objevit novou cestu.");
+            //}
+            
         }
     }
 }
